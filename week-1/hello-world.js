6d1a61d6e69f0cdd28a82a78396e9a0afe70ae7a
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-10-23 10:25:10 -0400

    Exercise 1.2

---
+++ week-1/hello-world.js
@@ -0,0 +1,8 @@
+/**
+ * Title: Exercise 1.2
+ * Author: Ethan Kenneth Winter
+ * Date: 23 October 2020
+ * Description: Write Hello World JS, and push to GitHub repo
+ */
+
+console.log("Hello World, my name is Ethan Kenneth Winter");
