9f4fba7853cea0aeccd1362b3a08e31fd5b73456
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-01 10:41:54 -0500

    Week 2
    Exercise 2.2

---
+++ week-2/winter-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   1 November 2020
+; Modified by: Ethan Kenneth Winter
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Exercise2-2-Header"));
