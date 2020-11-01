116545a1445a203dcb31bbf63d6373cc87a4906b
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-01 12:01:34 -0500

    Assignment 2.4

---
+++ week-2/winter-assignment2-4.js
@@ -0,0 +1,38 @@
+let header = require('./winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Assignment 2.4 Functions"));
+
+/**
+ * 6. Test each of these functions by calling them, passing in test values (yes this means you will need to create test variables), and outputting the results to the console window (yes, this means use the console.log() function to output the results).
+ * 7. Run and test the program and make sure your output matches what I have.
+ * 8. When you are happy with the output stage, commit, and push your code to GitHub.
+ */
+
+ // 1. Create a function called fullName with two parameters (first and last name) and a return the two parameters as one string.
+function fullName (firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+// 2. Create a function called dateWriter with three parameters (year, month, and day) and return a new date using the passed-in values.
+function dateWriter (year, month, day) {
+  let newDate = year + " " + month + " " + day;
+  return newDate;
+}
+
+// 3. Create a function called formatNumber with two parameters (number and numOfFixedPositions) and return the number with the number of fixed positions you specified in the second parameter.
+function formatNumber (number, numOfFixedPositions) {
+  return number.toFixed(numOfFixedPositions);
+}
+
+// 4. Create a function called convertToInt with one string parameter.  Parse the string parameter and return the value as an integer.
+function convertToInt() {
+
+}
+
+// 5. Create a function called convertToFloat with one string parameter.  Parse the string parameter and return the value as a float.
+function convertToFloat() {
+
+}
+
+// 6. Test each of these functions by calling them, passing in test values (yes this means you will need to create test variables), and outputting the results to the console window (yes, this means use the console.log() function to output the results).
+alert("Hello my name is " + fullName("Ethan Kenneth", "Winter")/n )
