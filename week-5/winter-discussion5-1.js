de46cc43d3c7687bc48c0ec019f90ea89af86247
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-19 14:46:49 -0500

    Discussion 5-1

---
+++ week-5/winter-discussion5-1.js
@@ -0,0 +1,17 @@
+/**
+ * Title: Discussion 5.1
+ * Author: Ethan Kenneth Winter
+ * Date: 19 November 2020
+ * Description: Create a keyed collection array with at least two errors
+ */
+
+let header = require('../week-2/winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Discussion 5.1 Advanced Arrays"));
+
+// pets keyed collection (one dog, one cat)
+let pets == new Map()
+pets['cat'] = cat
+pets['dog'] = dog
+
+console.log(pets)  // Map { cat: 'cat', dog: 'dog' }
