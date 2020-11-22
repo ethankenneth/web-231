aa075fa55fb9e179ed3486448ae6161b558fd5bd
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-22 10:59:46 -0500

    Exercise 5.3

---
+++ week-5/winter-exercise5-3.js
@@ -0,0 +1,62 @@
+/**
+ * Title: Exercise 5.3
+ * Author: Ethan Kenneth Winter
+ * Date: 22 November 2020
+ * Description: Create an array of composer objects and iterate over the array
+ */
+
+let header = require('../week-2/winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Exercise 5.3 Object Collections"));
+
+// Start program
+
+// Create array of composer objects
+let composers = [
+	{
+		firstName: "Ludwig van",
+		lastName: "Beethoven",
+		genre: "Classical",
+		rating: 8
+	},
+	{
+		firstName: "Wolfgang Amadeus",
+		lastName: "Mozart",
+		genre: "Classical",
+		rating: 10
+	},
+	{
+		firstName: "Johann Sebastian",
+		lastName: "Bach",
+		genre: "Classical",
+		rating: 9
+	},
+	{
+		firstName: "Joseph",
+		lastName: "Haydn",
+		genre: "Classical",
+		rating: 6
+	},
+	{
+		firstName: "Franz",
+		lastName: "Schubert",
+		genre: "Classical",
+		rating: 5
+	}
+];
+
+// console header
+console.log("-- Composers --");
+// iterate over artists
+composers.forEach(function (composer) {
+	console.log(
+		"Last Name: " +
+			composer.lastName +
+			", Genre: " +
+			composer.genre +
+			", Rating: " +
+			composer.rating
+	);
+});
+
+// End program
