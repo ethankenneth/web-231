0d7b07b0ed7fb748470bd9966c0666f90f8117c6
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-29 15:13:01 -0500

    Exercise 6.2 Exception Handling

---
+++ week-6/winter-exercise6-2.js
@@ -0,0 +1,33 @@
+/**
+ * Title: Exercise 6.2
+ * Author: Ethan Kenneth Winter
+ * Date: 29 November 2020
+ * Description: Create a try/catch with an error
+ */
+
+let header = require('../week-2/winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Exercise 6.2 Exception Handling"));
+
+// Start of program.
+
+// Try to see if x + y is 9
+try
+{
+  let x = 10;
+  let y = 100;
+  let sum = x + y;
+
+  if (sum == 9) throw "Your sum is 9";
+  console.log(sum);
+}
+catch (sum)
+{
+  console.log("Catch clause: Your sum is " + sum);
+}
+finally
+{
+  console.log("Finally clause: End of program ...");
+}
+
+// End of program.
