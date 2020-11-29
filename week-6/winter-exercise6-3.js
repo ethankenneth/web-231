0e3a54b75d34050f101b80b94b27a3634c289617
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-11-29 15:25:31 -0500

    Exercise 6.3

---
+++ week-6/winter-exercise6-3.js
@@ -0,0 +1,27 @@
+/**
+ * Title: Exercise 6.3
+ * Author: Ethan Kenneth Winter
+ * Date: 29 November 2020
+ * Description: Create a try/catch with an error
+ */
+
+let header = require('../week-2/winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Exercise 6.3 Object Literals"));
+
+// Start of program.
+
+// Create object literal for ticketing system.
+let ticket = {
+	id: 101,
+
+	name: "Help Desk Support",
+
+	requestor: "Bob Jones"
+};
+
+// Output the ticket information from the object literal.
+console.log("id: " + ticket.id + ", name: " + ticket.name + ", requester: " + ticket.requestor);
+// Instructions say "requestor" while github repo starter code says "requester."
+
+// End of progam.
