9b804c7820312513bb6a803f9ccd0328af8db3b7
Ethan Kenneth Winter <ethankenneth.winter@hearst.com>
2020-12-06 11:27:27 -0500

    Exercise 7.2 Constructor Functions

---
+++ week-7/winter-exercise7-2.js
@@ -0,0 +1,33 @@
+/**
+ * Title: Discussion 7.2
+ * Author: Ethan Kenneth Winter
+ * Date: 6 December 2020
+ * Description: Create an employee constructor.
+ */
+
+let header = require('../week-2/winter-header.js');
+
+console.log(header.display("Ethan Kenneth", "Winter", "Exercise 7.2 Constructor Functions"));
+
+
+// Employee constructor with id, first & last name and title.
+function Employee(id, firstName, lastName, title) {
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.title = title;
+}
+
+// Employee information for id, first & last name and title.
+var employees = [
+	new Employee(1, "Thomas", "Edison", "Software Engineer"),
+	new Employee(2, "Benjamin", "Franklin", "Programmer"),
+	new Employee(3, "Nikola", "Tesla", "Project Manager"),
+	new Employee(4, "Charles", "Babbage", "Product Manager"),
+	new Employee(5, "Alexander", "Bell", "Business Analyst")
+];
+
+// Loop over employees and display information.
+employees.forEach(function (Employee) {
+	console.log(this.employees);
+});
